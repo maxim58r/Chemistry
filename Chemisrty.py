4418e835a51b47fa7b0e25f320ffda45b42dd802
Serov Maxim <maxim58r@gmail.com>
2019-03-07 16:54:03 +0300

    don't separate words from numbers yet (пока не отделяет слова от цифр)

--- Chemisrty.py
+++ Chemisrty.py
@@ -2,24 +2,34 @@ from tkinter import *
 import re
 
 
-def window_open(label_1):
+def window_open(label_1):  # Всплывающее окно
     toplevel = Toplevel()
     label_new = Label(toplevel, text=label_1, height = 0, width = 100, wraplength = 700)
     label_new.pack()
 
-def output(event):
+def output(event):  # Обработка вводимого текста и неправильного ввода
 
     txt = entry1.get()
     try:
         if int(txt) > 0:
-            chem_element(int(txt))
+            chem_element_numer(int(txt))
         else:
             window_open("Введите положительный номер элемента")
     except ValueError:
         window_open("Введите корректный номер элемента")
 
+def chem_element_word(word):  # Поиск текста в текстовом файле по названию
 
-def chem_element(num):
+    text = "text.txt"
+    element_file = open(text, mode='r')
+    look_text = element_file.read()
+    number = word
+    textsearch = r"\d\:\{}:".format(number)
+    result = re.findall(textsearch, look_text)
+
+    return window_open(result)
+
+def chem_element_numer(num):  # Поиск текста в текстовом файле по номеру
 
     text = "text.txt"
     element_file = open(text, mode='r')
